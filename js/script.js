062fe21075edef3d8452d956ed16fa9a1bd3e374
Bulmaro Arellano <67934295+bulmaroarellano@users.noreply.github.com>
2022-08-06 20:13:36 -0500

    Fusion del menu y submenu para la version mobile

--- js/script.js
+++ js/script.js
@@ -1,9 +1,16 @@
 const itemEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector('.desktop-menu');
 
+const burgerMenuIcon = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu")
 
 itemEmail?.addEventListener("click", toggleSubMenu);
+burgerMenuIcon?.addEventListener("click", toggleMobileMenu);
 
 function toggleSubMenu(){
  desktopMenu?.classList.toggle('inactive'); // Alternará colocando y eliminando la clase inactive
+}
+
+function toggleMobileMenu(){
+    mobileMenu?.classList.toggle("inactive");
 }
